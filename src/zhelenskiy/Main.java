88533220040eef3ba1d199e760cd2d4eaf088baf
zhelenskiy <zhelenskiy2000@yandex.ru>
2021-09-08 01:46:06 +0300

    lambdas only

--- src/zhelenskiy/Main.java
+++ src/zhelenskiy/Main.java
@@ -14,12 +14,10 @@ public class Main {
     interface F4 {
         Object a(Object arg1, Object arg2, Object arg3, Object arg4);
     }
-    public static F1 y(F1 isGeneralCase, F2 whenTrue, F1 nextRecurParameter, Object otherwise) {
-        return (Object arg) -> ((F1) (Object x) -> ((F1)((F1)((F4)x).a(isGeneralCase, whenTrue, nextRecurParameter, otherwise)).a(arg)).a(x)).a((F4) (Object pred, Object then, Object next, Object other) -> (F1) ((Object arg_) -> !(boolean)((F1)pred).a(arg_) ? (F1) (Object x) -> other : (F1) (Object x) -> ((F2)then).a(arg_, ((F1)((F1)((F4)x).a(pred, then, next, other)).a(((F1)next).a(arg_))).a(x))));
-    }
+    public static F4 y = (Object isGeneralCase, Object whenTrue, Object nextRecurParameter, Object otherwise) -> (F1)((Object arg) -> ((F1) (Object x) -> ((F1)((F1)((F4)x).a(isGeneralCase, whenTrue, nextRecurParameter, otherwise)).a(arg)).a(x)).a((F4) (Object pred, Object then, Object next, Object other) -> (F1) ((Object arg_) -> !(boolean)((F1)pred).a(arg_) ? (F1) (Object x) -> other : (F1) (Object x) -> ((F2)then).a(arg_, ((F1)((F1)((F4)x).a(pred, then, next, other)).a(((F1)next).a(arg_))).a(x)))));
 
     public static void main(String[] args) {
-        var factorial = y(x -> (int)x > 0, (cur, got) -> (int)cur * (int) got, x -> (int)x - 1, 1);
+        var factorial = y.a((F1)(x -> (int)x > 0), (F2)((cur, got) -> (int)cur * (int) got), (F1)(x -> (int)x - 1), 1);
         class Pair {
             final int item1, item2;
 
@@ -28,16 +26,16 @@ public class Main {
                 this.item2 = item2;
             }
         }
-        var fibonacci = y(x -> (int)x > 0, (cur, pair) -> {
+        var fibonacci = y.a((F1)(x -> (int)x > 0), (F2)((cur, pair) -> {
             var p = (Pair) pair;
             return new Pair(p.item2, p.item1 + p.item2);
-        }, x -> (int)x - 1, new Pair(0, 1));
+        }), (F1)(x -> (int)x - 1), new Pair(0, 1));
         for (int i = 0; i < 10; i++) {
-            System.out.println(factorial.a(i));
+            System.out.println(((F1)factorial).a(i));
         }
         System.out.println();
         for (int i = 0; i < 10; i++) {
-            System.out.println(((Pair)fibonacci.a(i)).item1);
+            System.out.println(((Pair)((F1)fibonacci).a(i)).item1);
         }
     }
 }
